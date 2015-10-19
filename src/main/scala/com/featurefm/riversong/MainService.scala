034245ec656b8363d3971367877115ada79badb8
ymeymann <why.me.why.man@gmail.com>
2015-10-19 19:00:58 +0300

    Lifecycle routes are always present and rejections are logged

--- src/main/scala/com/featurefm/riversong/MainService.scala
+++ src/main/scala/com/featurefm/riversong/MainService.scala
@@ -3,13 +3,13 @@ package com.featurefm.riversong
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.RouteResult.Complete
+import akka.http.scaladsl.server.RouteResult.{Rejected, Complete}
+import akka.http.scaladsl.server.{Directives, Route}
 import akka.stream.ActorMaterializer
 import com.featurefm.riversong.metrics.reporting.MetricsReportingManager
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.directives._
-import akka.http.scaladsl.server.directives.DebuggingDirectives._
-import akka.http.scaladsl.server.Route
+import Directives._
 
 /**
  * Created by yardena on 9/20/15.
@@ -29,16 +29,19 @@ abstract class MainService(val name: String = "Spoilers") extends App with Confi
   private def wrapper(req: HttpRequest): Any => Option[LogEntry] = {
     case Complete(res) =>
       Some(LogEntry(s"${req.method.value} ${req.uri} ~> ${res.status}", Logging.InfoLevel))
-    case _ => None // other kind of responses
+    case Rejected(seq) if seq.isEmpty => Some(LogEntry(s"${req.method.value} ${req.uri} ~> ${StatusCodes.NotFound}", Logging.InfoLevel))
+    case Rejected(_) => Some(LogEntry(s"${req.method.value} ${req.uri} ~> Rejected", Logging.InfoLevel))
   }
 
-  private lazy val rawRoutes: Route = assembly.routes
+  private lazy val rawRoutes: Route = assembly.routes ~ assembly.lifecycle.routes
   private lazy val routes: Route =
     if (config.getBoolean("akka.http.server.request_log"))
       logRequestResult(wrapper _)(rawRoutes)
     else
       rawRoutes
-  Http().bindAndHandle(routes, host, port)
+  Http().bindAndHandle(routes, host, port) onSuccess {
+    case bind: Http.ServerBinding => log.info(s"Server ${bind.localAddress} started")
+  }
 
   // Start the metrics reporters
   system.actorOf(MetricsReportingManager.props())
