be237e739941a9573e2af4849a8a105b6f32c711
ymeymann <why.me.why.man@gmail.com>
2015-10-19 13:09:13 +0300

    support request logging

--- src/main/scala/com/featurefm/riversong/MainService.scala
+++ src/main/scala/com/featurefm/riversong/MainService.scala
@@ -3,7 +3,9 @@ package com.featurefm.riversong
 import akka.actor.ActorSystem
 import akka.event.Logging
 import com.featurefm.riversong.metrics.reporting.MetricsReportingManager
+import spray.http.{HttpResponse, HttpRequest}
 import spray.routing.SimpleRoutingApp
+import spray.routing.directives.LogEntry
 
 /**
  * Created by yardena on 9/20/15.
@@ -17,8 +19,23 @@ abstract class MainService(val name: String = "Spoilers") extends SimpleRoutingA
 
   def assembly:ServiceAssembly
 
+  //from https://github.com/knoldus/spray-akka-starter/blob/master/src/main/scala/com/knoldus/hub/StartHub.scala
+  private def requestMethodAndResponseStatusAsInfo(req: HttpRequest): Any => Option[LogEntry] = {
+    case res: HttpResponse =>
+      //      val verbose = config.getBoolean("spray.request_log_verbose")
+      Some(LogEntry(
+        //        if (verbose) s"${req.method} ${req.uri} ${req.entity.data.asString} ~> ${res.message.status} ${res.entity.data.asString}"
+        //        else
+        s"${req.method} ${req.uri} ~> ${res.message.status}", Logging.InfoLevel))
+    case _ => None // other kind of responses
+  }
+
   startServer(interface = host, port = port) {
-    assembly.routes
+    val rawRoutes = assembly.routes
+    if (config.getBoolean("spray.request_log"))
+      logRequestResponse(requestMethodAndResponseStatusAsInfo _)(rawRoutes)
+    else
+      rawRoutes
   }
 
   // Start the metrics reporters
