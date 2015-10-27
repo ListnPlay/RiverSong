ed869cd5faf0d0690a41e9dd79514c20c426118b
ymeymann <yardenam@feature.fm>
2015-10-27 20:49:27 +0200

    add implicit materializer in BaseRouting

--- src/main/scala/com/featurefm/riversong/routes/BaseRouting.scala
+++ src/main/scala/com/featurefm/riversong/routes/BaseRouting.scala
@@ -1,13 +1,22 @@
 package com.featurefm.riversong.routes
 
+import akka.event.Logging
 import akka.http.scaladsl.model.{StatusCode, StatusCodes}
 import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
 import com.featurefm.riversong.metrics.Instrumented
+import org.json4s.jackson.Serialization
 
 /**
  * Created by yardena on 8/6/15.
  */
 trait BaseRouting extends Instrumented {
+
+  implicit val serialization = Serialization
+  implicit val context = system.dispatcher
+  implicit val materializer = ActorMaterializer()
+
+  val log = Logging(system, getClass)
 
   def status(e: Throwable, code: StatusCode): StatusCode = if (e.isInstanceOf[IllegalArgumentException]) code else StatusCodes.InternalServerError
 
