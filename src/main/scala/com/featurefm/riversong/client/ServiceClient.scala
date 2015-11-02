0863cf8b46fe5912144336678f9cb5d86fb3f1df
Yardena Meymann <yardenam@feature.fm>
2015-11-02 10:16:35 +0200

    https

--- src/main/scala/com/featurefm/riversong/client/ServiceClient.scala
+++ src/main/scala/com/featurefm/riversong/client/ServiceClient.scala
@@ -5,7 +5,7 @@ import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.stream.Materializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.featurefm.riversong.metrics.Instrumented
 import org.json4s.jackson.Serialization
 import akka.http.scaladsl.client.RequestBuilding._
@@ -16,17 +16,20 @@ import scala.concurrent.Future
 /**
  * Created by yardena on 11/1/15.
  */
-class ServiceClient(host: String, port: Int)(implicit val system: ActorSystem, implicit val mat: Materializer) extends Instrumented {
+class ServiceClient private (flow: => Flow[HttpRequest, HttpResponse, Any])(implicit val system: ActorSystem, implicit val mat: Materializer)
+  extends Instrumented {
+
+//  def this(host: String, port: Int) {
+//    this(Http().outgoingConnection(host, port))
+//  }
 
   implicit val executor = system.dispatcher
   implicit val serialization = Serialization // or native.Serialization
 
   val log = Logging(system, getClass)
 
-  private lazy val connectionFlow = Http().outgoingConnection(host, port) //Flow[HttpRequest, HttpResponse, Any]
-
   def send(request: HttpRequest): Future[HttpResponse] = timeEventually(s"${request.method.value} ${request.uri}") {
-    Source.single(request).via(connectionFlow).runWith(Sink.head)
+    Source.single(request).via(flow).runWith(Sink.head)
   }
 
   def status: Future[Boolean] = send(Get("/status")) map { _.status match {
@@ -34,4 +37,16 @@ class ServiceClient(host: String, port: Int)(implicit val system: ActorSystem, i
     case _ => false
   }}
 
+}
+
+object ServiceClient {
+
+  def http(host: String, port: Int = 80)(implicit system: ActorSystem, mat: Materializer): ServiceClient = {
+    require(host.startsWith("http://") || host.indexOf("://") < 0, "Protocol must be HTTP")
+    new ServiceClient(Http().outgoingConnection(host, port))
+  }
+  def https(host: String, port: Int = 443)(implicit system: ActorSystem, mat: Materializer): ServiceClient = {
+    require(host.startsWith("https://") || host.indexOf("://") < 0, "Protocol must be HTTPS")
+    new ServiceClient(Http().outgoingConnectionTls(host, port))
+  }
 }
