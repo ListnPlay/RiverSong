579b15795a3b81ca1708f2aa152d5b75dbb32a11
Yardena Meymann <yardenam@feature.fm>
2016-12-05 19:59:46 +0200

    support BigMoney

--- src/main/scala/com/featurefm/riversong/Json4sProtocol.scala
+++ src/main/scala/com/featurefm/riversong/Json4sProtocol.scala
@@ -1,6 +1,6 @@
 package com.featurefm.riversong
 
-import com.featurefm.riversong.money.MoneySerializer
+import com.featurefm.riversong.money.{BigMoneySerializer, MoneySerializer}
 import de.heikoseeberger.akkahttpjson4s.Json4sSupport
 import org.json4s._
 
@@ -9,7 +9,7 @@ import org.json4s._
  */
 trait Json4sProtocol extends Json4sSupport {
   implicit val serialization = jackson.Serialization
-  def frameworkFormats: Formats = DefaultFormats ++ org.json4s.ext.JodaTimeSerializers.all + org.json4s.ext.UUIDSerializer + new MoneySerializer
+  def frameworkFormats: Formats = DefaultFormats ++ org.json4s.ext.JodaTimeSerializers.all + org.json4s.ext.UUIDSerializer + new MoneySerializer + new BigMoneySerializer
   implicit val json4sJacksonFormats: Formats = frameworkFormats
 }
 
