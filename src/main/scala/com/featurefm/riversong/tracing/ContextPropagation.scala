ad78f8fcae8ce16c86d02981ec7cd008d53a0e4f
lshapsa <lshapsa@feature.fm>
2025-03-26 13:17:41 +0200

    add new distributed headers

--- src/main/scala/com/featurefm/riversong/tracing/ContextPropagation.scala
+++ src/main/scala/com/featurefm/riversong/tracing/ContextPropagation.scala
@@ -8,6 +8,6 @@ case class ContextPropagation(
 
 object ContextPropagation{
   val dtHeadersSet = Set(
-    "x-request-id", "x-b3-traceid", "x-b3-spanid", "x-b3-parentspanid", "x-b3-sampled", "x-b3-flags", "x-ot-span-context", "x-cloud-trace-context", "traceparent", "grpc-trace-bin"
+    "x-request-id", "x-b3-traceid", "x-b3-spanid", "x-b3-parentspanid", "x-b3-sampled", "x-b3-flags", "x-ot-span-context", "x-cloud-trace-context", "traceparent", "grpc-trace-bin", "x-ffm-user-id", "x-ffm-impersonated-to-user-id", "x-ffm-impersonated-from-admin"
   )
 }
