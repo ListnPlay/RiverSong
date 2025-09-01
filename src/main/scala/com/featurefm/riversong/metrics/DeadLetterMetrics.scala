3be1320697cf27488cdf93a8d2c18b1f1249c4c2
lshapsa <lshapsa@feature.fm>
2025-09-01 14:13:17 +0300

    update version to 0.12.29 and enhance DeadLetterMetrics with safe class name retrieval

--- src/main/scala/com/featurefm/riversong/metrics/DeadLetterMetrics.scala
+++ src/main/scala/com/featurefm/riversong/metrics/DeadLetterMetrics.scala
@@ -10,11 +10,25 @@ class DeadLetterMetrics extends Actor with Instrumented {
   override implicit lazy val system: ActorSystem = context.system
   override lazy val metricBaseName: MetricName = MetricName("dead-letters")
 
+  private def getSafeClassName(obj: Any): String = {
+    try {
+      val simpleName = obj.getClass.getSimpleName
+      if (simpleName != null && simpleName.nonEmpty) {
+        simpleName
+      } else {
+        obj.getClass.getName.split('.').lastOption.getOrElse("UnknownClass")
+      }
+    } catch {
+      case _: InternalError | _: Exception =>
+        // Fallback to full class name, then extract the last part
+        obj.getClass.getName.split('.').lastOption.getOrElse("UnknownClass")
+    }
+  }
+
   def receive = {
     case DeadLetter(msg, from, to) =>
-      metrics.counter(s"message.${msg.getClass.getSimpleName}").inc(1)
+      metrics.counter(s"message.${getSafeClassName(msg)}").inc(1)
       metrics.counter(s"from.${from.path.name}").inc(1)
       metrics.counter(s"to.${to.path.name}").inc(1)
   }
-
 }
